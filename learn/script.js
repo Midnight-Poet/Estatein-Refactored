be42669d9c89f343aa19427bec0d86d11492da0c
Midnight-Poet <kaydeymyrtle@gmail.com>
2024-07-13 13:59:19 +0100

    Commit

--- learn/script.js
+++ learn/script.js
@@ -1,83 +1,43 @@
-
-function num1() {
-    numbers = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-    ]
-        first = document.getElementById('play').innerText = numbers[0]
-        
-    
-    // console.log(numbers[0]);
+function total() {
+    function display() {
+        let show = document.getElementById("input").value;
+        console.log(show);
+    }
+    function display() {
+        let show = document.getElementById("input1").value;
+        console.log(show);
+    }
+    function display() {
+        let show = document.getElementById("input2").value;
+        console.log(show);
+    }
+    function display() {
+        let show = document.getElementById("input3").value;
+        console.log(show);
+    }
+    function display() {
+        let show = document.getElementById("input4").value;
+        console.log(show);
+    }
+    function display() {
+        let show = document.getElementById("input5").value;
+        console.log(show);
+    }
+    function display() {
+        let show = document.getElementById("input6").value;
+        console.log(show);
+    }
+    function display() {
+        let show = document.getElementById("input7").value;
+        console.log(show);
+    }
+    function display() {
+        let show = document.getElementById("input8").value;
+        console.log(show);
+    }
+    function display() {
+        let show = document.getElementById("input9").value;
+        console.log(show);
+    }
 }
-function num2() {
-    numbers = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-    ]
-        first = document.getElementById('play').innerText = numbers[1]
-        
-    
-    // console.log(numbers[0]);
-}function num3() {
-    numbers = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-    ]
-        first = document.getElementById('play').innerText = numbers[2]
-        
-    
-    // console.log(numbers[0]);
-}function num4() {
-    numbers = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-    ]
-        first = document.getElementById('play').innerText = numbers[3]
-        
-    
-    // console.log(numbers[0]);
-}function num5() {
-    numbers = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-    ]
-        first = document.getElementById('play').innerText = numbers[4]
-        
-    
-    // console.log(numbers[0]);
-}function num6() {
-    numbers = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-    ]
-        first = document.getElementById('play').innerText = numbers[5]
-        
-    
-    // console.log(numbers[0]);
-}function num7() {
-    numbers = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-    ]
-        first = document.getElementById('play').innerText = numbers[6]
-        
-    
-    // console.log(numbers[0]);
-}function num8() {
-    numbers = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-    ]
-        first = document.getElementById('play').innerText = numbers[7]
-        
-    
-    // console.log(numbers[0]);
-}function num9() {
-    numbers = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-    ]
-        first = document.getElementById('play').innerText = numbers[8]
-        
-    
-    // console.log(numbers[0]);
-}function num0() {
-    numbers = [
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0
-    ]
-        first = document.getElementById('play').innerText = numbers[9]
-        
-    
-    // console.log(numbers[0]);
-}
+total()
