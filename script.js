2c83cd6e6f8100ecef377b075c13f5f9e40a10fb
Midnight-Poet <kaydeymyrtle@gmail.com>
2024-08-15 21:02:37 +0100

    Final Push Hopefully

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
-let slideContent = document.getElementById("slide").children
-let prev = document.getElementById("prev")
-let next = document.getElementById("next")
-let id = document.getElementById("id")
-let total = document.getElementById("total")
-total.innerHTML = "0" + slideContent.length
-let x = 1
-id.innerHTML = "0" + x
+let slideContent = document.querySelectorAll(".sub-divs")
+let prev = document.querySelectorAll("#prev")
+let next = document.querySelectorAll("#next")
+let counter = document.querySelectorAll("#counter")
+let total = document.querySelectorAll("#total")
+let subCounter = document.querySelectorAll(".counter")
+let subTotal = document.querySelectorAll(".total")
 
-next.addEventListener('click', ()=> {
-    document.getElementById("slide").appendChild(slideContent[0])
-    
-    x = x + 1
-    if (x > slideContent.length) {
-        x = 1
+
+let navpop = document.querySelector(".menu")
+let closeNav = document.querySelector(".close")
+let nav = document.querySelector(".nav")
+
+
+navpop.addEventListener('click', () => {
+    if (nav.className === "nav") {
+        nav.className += " navget"
+    } else {
+        nav.className = "nav"
     }
-    id.innerHTML = "0" + x
-    
-    
+    // nav.appendChild(document.querySelector('.navget'))
+
+    // nav.style.display = "block"
 })
-prev.addEventListener('click', () => {
-    document.getElementById("slide").prepend(slideContent[slideContent.length - 1])
-    x = x - 1
-    if (x == 0) {
-        x = slideContent.length
+closeNav.addEventListener('click', () => {
+    if (nav.className === "nav navget") {
+        nav.className = "nav"
+    } else {
+        nav.className ="nav navget"
     }
-    id.innerHTML = "0" + x
 })
+
+
+
+for (let i = 0; i < 5; i++) {
+
+    total[i].innerHTML = "0" + slideContent[i].children.length
+    let x = 1
+    counter[i].innerHTML = "0" + x
+    subCounter[i].innerHTML = counter[i].innerHTML
+    subTotal[i].innerHTML = total[i].innerHTML
+
+    next[i].addEventListener('click', ()=> {
+            
+        slideContent[i].appendChild(slideContent[i].children[0])
+        x = x + 1
+        if (x > slideContent[i].children.length) {
+            x = 1
+        }
+        counter[i].innerHTML = "0" + x
+        subCounter[i].innerHTML = counter[i].innerHTML
+        subTotal[i].innerHTML = total[i].innerHTML
+        
+    })
+    prev[i].addEventListener('click', () => {
+        slideContent[i].prepend(slideContent[i].children[slideContent[i].children.length - 1])
+        x = x - 1
+        if (x == 0) {
+            x = slideContent[i].children.length
+        }
+        counter[i].innerHTML = "0" + x
+        subCounter[i].innerHTML = counter[i].innerHTML
+        subTotal[i].innerHTML = total[i].innerHTML
+    }) 
+}
+
+// NAviagtion Settings
+
 
