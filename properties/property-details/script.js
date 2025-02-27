4ae21e0ea921192a8f74978a5b8e1255bea8df26
= <=iamustapha213@gmail.com>
2025-02-27 09:53:17 +0100

    Conclusion of Estatein i hope

--- properties/property-details/script.js
+++ properties/property-details/script.js
@@ -1,3 +1,82 @@
+// Loading
+if (document.onloadeddata = false) {
+	gsap.to('.div', {
+		display: 'block',
+	})
+	
+	
+} else {
+	gsap.to('.div', {
+		display: 'none',
+	})
+}
+
+
+
+
+
+
+// Scr0ll Trigger Animation
+gsap.registerPlugin(ScrollTrigger);
+// First Section
+// let firstSection = document.querySelector('.hero-section')
+
+// gsap.from(firstSection.firstElementChild, {
+// 	ScrollTrigger: {
+// 		trigger: firstSection,
+// 		start: "top 50%",
+// 	},
+// 	y: 200,
+// 	opacity: 0,
+// 	duration: 1
+// })
+// gsap.from(firstSection.children[1], {
+// 	ScrollTrigger: {
+// 		trigger: firstSection,
+// 		start: "top 50%",
+// 	},
+// 	y: 100,
+// 	opacity: 0,
+// 	duration: 1
+// })
+
+let subs = document.querySelectorAll('.pop-up')
+let subDiv = gsap.utils.toArray(subs)
+let minor = document.querySelectorAll('.sub-sec')
+let minors = gsap.utils.toArray(minor)
+
+subDiv.forEach(element => {
+	gsap.from(element.children, {
+		scrollTrigger: {
+			trigger: element.children,
+			start: 'top 100%',
+		},
+		markers: true,
+		y: 200,
+		opacity: 0,
+		duration: 1,
+		stagger: {
+			each: 0.3
+		}
+	})
+});
+minors.forEach(element => {
+	gsap.from(element.children, {
+		scrollTrigger: {
+			trigger: element.children,
+			start: 'top 100%',
+		},
+		markers: true,
+		y: 200,
+		opacity: 0,
+		duration: 1,
+		stagger: {
+			each: 0.3,
+		}
+	})
+});
+
+
 let navbar = document.querySelector("nav");
 let hideMenu = document.querySelector(".close-btn");
 let showMenu = document.querySelector(".menu-btn")
--- script.js
+++ script.js
@@ -1,8 +1,90 @@
+// Loading
+if (document.onloadeddata = false) {
+	gsap.to('.div', {
+		display: 'block',
+	})
+	
+	
+} else {
+	gsap.to('.div', {
+		display: 'none',
+	})
+}
+
+
+
+
+
+
+// Scr0ll Trigger Animation
+gsap.registerPlugin(ScrollTrigger);
+// First Section
+let firstSection = document.querySelector('.first-section')
+
+gsap.from(firstSection.firstElementChild, {
+	ScrollTrigger: {
+		trigger: firstSection,
+		start: "top 50%",
+	},
+	y: 200,
+	opacity: 0,
+	duration: 1
+})
+gsap.from(firstSection.children[1], {
+	ScrollTrigger: {
+		trigger: firstSection,
+		start: "top 50%",
+	},
+	y: 100,
+	opacity: 0,
+	duration: 1
+})
+
+let subs = document.querySelectorAll('.pop-up')
+let subDiv = gsap.utils.toArray(subs)
+let minor = document.querySelectorAll('.sub-divs')
+let minors = gsap.utils.toArray(minor)
+
+subDiv.forEach(element => {
+	gsap.from(element.children, {
+		scrollTrigger: {
+			trigger: element.children,
+			start: 'top 100%',
+		},
+		markers: true,
+		y: 200,
+		opacity: 0,
+		duration: 1,
+		stagger: {
+			each: 0.3
+		}
+	})
+});
+minors.forEach(element => {
+	gsap.from(element.children, {
+		scrollTrigger: {
+			trigger: element.children,
+			start: 'top 100%',
+		},
+		markers: true,
+		y: 200,
+		opacity: 0,
+		duration: 1,
+		stagger: {
+			each: 0.3,
+		}
+	})
+});
+
+
+// Navigation Menu
+
 let navbar = document.querySelector("nav");
 let hideMenu = document.querySelector(".close-btn");
 let showMenu = document.querySelector(".menu-btn")
 
 let screenWidth;
+
 
 setInterval(() => {
   screenWidth = window.innerWidth;
@@ -12,229 +94,27 @@ setInterval(() => {
     });
   }
 }, 100);
-
 showMenu.addEventListener("click", () => {
-  gsap.set(".res-nav", {
-    x: 500,
-    display: "none",
-  });
-  gsap.to(".res-nav", {
-    x: 0,
-    display: "block",
-    duration: 0.5,
-  });
+	gsap.set(".res-nav", {
+		x: 500,
+		display: "none",
+	});
+	gsap.to(".res-nav", {
+		x: 0,
+		display: "block",
+		duration: 0.5,
+	});
 });
-
+  
 hideMenu.addEventListener("click", () => {
-  gsap.to(".res-nav", {
-    x: 500,
-    display: "none",
-    duration: 0.5,
-  });
+	gsap.to(".res-nav", {
+		x: 500,
+		display: "none",
+		duration: 0.5,
+	});
 });
 
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.from(".img1", {
-  scrollTrigger: {
-    // toggleActions: "play reverse play reverse",// onEnter onLeave onENterBack onLeaveBack
-    trigger: ".first-section",
-  },
-  opacity: 0,
-  // duration: 1,
-  delay: 0.5,
-  filter: "blur(33px)",
-  ease: "power1.in",
-});
-
-gsap.from(".txt1", {
-  scrollTrigger: {
-    trigger: ".first-section",
-    toggleActions: "play reset play reset", // onEnter onLeave onENterBack onLeaveBack
-  },
-  x: 200,
-  opacity: 0,
-  duration: 2,
-  ease: "elastic",
-});
-gsap.from(".sdiv", {
-  scrollTrigger: {
-    trigger: ".first-section",
-    toggleActions: "play reset play reset",
-  },
-  delay: 0.5,
-  x: -300,
-  ease: "elastic",
-  opacity: 0,
-  duration: 2,
-  stagger: {
-    amount: 1,
-    from: 2,
-  },
-});
-gsap.from(".div-col-1", {
-  scrollTrigger: {
-    trigger: ".second-section",
-    start: "top 75%",
-    toggleActions: "play reverse play reverse",
-    end: "top 50%",
-    scrub: 2,
-  },
-  y: 100,
-  opacity: 0,
-  stagger: {
-    amount: 0.8,
-    each: 0.2,
-    from: 0,
-  },
-});
-
-let star = document.querySelectorAll(".star");
-let sub = document.querySelectorAll(".sec-obj");
-
-// 08162596865
-
-// gsap.utils.toArray(star).forEach((star) => {
-//   gsap.set(star.children, {
-//     y: 50,
-//     x: -200,
-//     scale: 2,
-//   });
-//   gsap.set('.div-col-2', {
-//     y: 50,
-//     opacity: 0
-//   });
-  
-
-//   let tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: star,
-//       start: "top 55%",
-//       end: "bottom 35%",
-//     //   pin: true,
-//     //   pinSpacing: 10,
-//       markers: true,
-//     //   scrub: 3
-//     },
-//   });
-//   tl.to(star.children, {
-//     x: 0,
-//     duration: 1,
-//     stagger: {
-//       amount: 0.5,
-//       from: 0,
-//     },
-//   }).to(star.children, {
-//     y: 0,
-//     scale: 1,
-//     rotate: -360,
-//     duration: 1,
-//     stagger: {
-//       amount: 0.5,
-//       from: 0,
-//     },
-//   }).to(star.children, {
-//     rotate: -360,
-//     repeat: 2,
-//     duration: 1
-//   }).to('.div-col-2', {
-//     opacity: 1,
-//     y: 0,
-//     duration: 1
-//   })
-
-
-// });
-
-for (let i = 0; i < star.length; i++) {
-  gsap.set(star[i].children, {
-    y: 50,
-    x: -200,
-    scale: 2,
-  });
-  gsap.set(sub[i].children[1], {
-    y: 50,
-    opacity: 0
-  });
-  gsap.set(sub[i].children[3], {
-    y: 50,
-    opacity: 0
-  });
-  gsap.set(sub[i].children[2].children, {
-    y: 50,
-    opacity: 0
-  });
-  
-  
-  let tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: star[i],
-      start: "top 55%",
-      end: "bottom 35%",
-      toggleActions: "play none play reverse",
-},
-  });
-  tl.to(star[i].children, {
-    x: 0,
-    // duration: 1,
-    stagger: {
-      amount: 0.5,
-      from: 0,
-    },
-  }).to(star[i].children, {
-    y: 0,
-    scale: 1,
-    rotate: -360,
-    // duration: 1,
-    stagger: {
-      amount: 0.5,
-      from: 0,
-    },
-  }).to(sub[i].children[1], {
-    opacity: 1,
-    y: 0,
-    duration: 0.7
-  }).to(sub[i].children[2].children, {
-    y: 0,
-    opacity: 1,
-    stagger: {
-      amount: 0.6,
-	    each: 0.2,
-      from: 0
-    }
-  }).to(sub[i].children[3], {
-    // scrollTrigger: sub[i].children[3],
-    duration: 0.4,
-    opacity: 1,
-    y: 0
-  }).to(star[i].children, {
-    rotate: 360,
-    duration: 10,
-    repeat: -1
-  })
-
- 
-}
-gsap.from(sub[star.length], {
-	scrollTrigger: {
-		trigger: sub[star.length], 
-		start: "top 75%",
-      	end: "bottom 35%",
-	},
-	opacity: 0,
-	y: 50
-})
-
-gsap.from('footer', {
-	scrollTrigger: {
-		trigger: 'footer', 
-		start: "top 75%",
-      	end: "bottom 35%",
-	},
-	opacity: 0,
-	y: 50
-  })
-
+// Sliders
 let slideContent = document.querySelectorAll(".sub-divs");
 let prev = document.querySelectorAll("#prev");
 let next = document.querySelectorAll("#next");
